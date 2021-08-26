4853bf0d8a325ede0fda31edbc58d603c216f8f6
Annelies-P <annelies.pauwels@gmail.com>
2021-08-26 14:48:49 +0200

    improve sources query and fix task status updates

--- sparql-queries/press-release.sparql-queries.js
+++ sparql-queries/press-release.sparql-queries.js
@@ -1,44 +1,49 @@
 import { querySudo as query } from '@lblod/mu-auth-sudo';
 import { mapBindingValue } from '../helpers/generic-helpers';
 import { sparqlEscapeUri } from 'mu';
-import { PREFIXES, VERZENDLIJSTEN_PUBLICATION_CHANNEL } from './sparql-constants';
+import { PREFIXES, VERZENDLIJSTEN_PUBLICATION_CHANNEL, PUBLIC_GRAPH } from './sparql-constants';
 
 export async function getPressReleaseSources(pubTask) {
 
     const q = await query(`
         ${PREFIXES}
 
-        SELECT ?source ?fullName ?function ?telephone ?mobile ?email ?organization
-        WHERE {
-            ${sparqlEscapeUri(pubTask.pressRelease)}    a                   fabio:PressRelease;
-                                                        dct:source          ?source.
-            ?source                                     a                   ebucore:Contact;
-                                                        vcard:fn            ?fullName.
+        SELECT ?source ?fullName ?function ?telephone ?mobile ?email ?organization WHERE {
+            GRAPH ${sparqlEscapeUri(pubTask.graph)} {
+                ${sparqlEscapeUri(pubTask.pressRelease)}    a                   fabio:PressRelease;
+                                                            dct:source          ?source.
+                ?source                                     a                   ebucore:Contact;
+                                                            vcard:fn            ?fullName.
 
-            OPTIONAL { ?source                          vcard:role          ?function }
+                OPTIONAL { ?source                          vcard:role          ?function }
+
+                OPTIONAL {
+                    ?source             vcard:hasTelephone        ?telephoneURI.
+                    ?telephoneURI       a                         vcard:Voice;
+                                        vcard:hasValue            ?telephone;
+                                        ext:publicationChannel    ${sparqlEscapeUri(VERZENDLIJSTEN_PUBLICATION_CHANNEL)}.
+                }
+                OPTIONAL {
+                    ?source            ext:hasMobile              ?mobileURI.
+                    ?mobileURI         a                          vcard:Cell;
+                                    vcard:hasValue             ?mobile;
+                                    ext:publicationChannel     ${sparqlEscapeUri(VERZENDLIJSTEN_PUBLICATION_CHANNEL)}.
+                }
+                OPTIONAL {
+                    ?source            vcard:hasEmail             ?emailURI.
+                    ?emailURI          a                          vcard:Email;
+                                    vcard:hasValue             ?email;
+                                    ext:publicationChannel     ${sparqlEscapeUri(VERZENDLIJSTEN_PUBLICATION_CHANNEL)}.
+                }
+            }
             OPTIONAL{
-                ?organizationURI                        a                   vcard:Organization;
-                                                        org:hasMember       ?source;
+                GRAPH <${PUBLIC_GRAPH}> {
+                    ?organizationURI                    a                   vcard:Organization;
                                                         vcard:fn            ?organization.
-            }
-
-            OPTIONAL {
-                ?source             vcard:hasTelephone        ?telephoneURI.
-                ?telephoneURI       a                         vcard:Voice;
-                                    vcard:hasValue            ?telephone;
-                                    ext:publicationChannel    ${sparqlEscapeUri(VERZENDLIJSTEN_PUBLICATION_CHANNEL)}.
-            }
-            OPTIONAL {
-                ?source            ext:hasMobile              ?mobileURI.
-                ?mobileURI         a                          vcard:Cell;
-                                   vcard:hasValue             ?mobile;
-                                   ext:publicationChannel     ${sparqlEscapeUri(VERZENDLIJSTEN_PUBLICATION_CHANNEL)}.
-            }
-            OPTIONAL {
-                ?source            vcard:hasEmail             ?emailURI.
-                ?emailURI          a                          vcard:Email;
-                                   vcard:hasValue             ?email;
-                                   ext:publicationChannel     ${sparqlEscapeUri(VERZENDLIJSTEN_PUBLICATION_CHANNEL)}.
+                }
+                GRAPH ${sparqlEscapeUri(pubTask.graph)} {
+                    ?organizationURI                    org:hasMember       ?source.
+                }
             }
         }
     `);
