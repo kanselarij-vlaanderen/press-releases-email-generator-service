3e07414f4537a4c33d1ffeacdfb7e7bdd7735659
Annelies-P <annelies.pauwels@gmail.com>
2021-10-22 22:34:19 +0200

    code refactoring

---
+++ config.js
@@ -0,0 +1,7 @@
+const PUBLIC_GRAPH = 'http://mu.semte.ch/graphs/public';
+const VERZENDLIJSTEN_PUBLICATION_CHANNEL = 'http://themis.vlaanderen.be/id/publicatiekanaal/c184f026-feaa-4899-ba06-fd3a03df599c';
+
+export {
+  VERZENDLIJSTEN_PUBLICATION_CHANNEL,
+  PUBLIC_GRAPH
+}
