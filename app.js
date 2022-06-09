e0f4d0af204d3b3aadb7092ce4a0a5934f00b7f0
Erika Pauwels <erika.pauwels@gmail.com>
2022-06-09 23:28:29 +0200

    Fetch mail template to use from triplestore

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ import { app, errorHandler } from 'mu';
 import { getNotStartedPublicationTasks, TASK_ONGOING_STATUS } from './lib/publication-task';
 
 app.post('/delta', async function (req, res, next) {
-    console.log("Processing deltas for email...");
+  console.log("Processing deltas for email...");
 
-    const publicationTasks = await getNotStartedPublicationTasks();
+  const publicationTasks = await getNotStartedPublicationTasks();
 
-    if (publicationTasks) {
-      console.log(`Found ${publicationTasks.length} publication tasks to be processed.`);
-      for (const publicationTask of publicationTasks) {
-        await publicationTask.persistStatus(TASK_ONGOING_STATUS);
-      };
-      res.sendStatus(202);
-      for (const publicationTask of publicationTasks) {
-        await publicationTask.process();
-      };
-    } else {
-      console.log(`No publication tasks found to be processed.`);
-      return res.status(200).end();
-    }
-  });
+  if (publicationTasks) {
+    console.log(`Found ${publicationTasks.length} publication tasks to be processed.`);
+    for (const publicationTask of publicationTasks) {
+      await publicationTask.persistStatus(TASK_ONGOING_STATUS);
+    };
+    res.sendStatus(202);
+    for (const publicationTask of publicationTasks) {
+      await publicationTask.process();
+    };
+  } else {
+    console.log(`No publication tasks found to be processed.`);
+    return res.status(200).end();
+  }
+});
 
 // use mu errorHandler middleware.
 app.use(errorHandler);
